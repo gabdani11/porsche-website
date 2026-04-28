5c7774686056080717a11248833ce0fef0ed937b
gabdani11 <gabrielang56799@gmail.com>
2026-04-28 16:36:48 +0530

    added hero section

--- src/sections/hero/Hero.jsx
+++ src/sections/hero/Hero.jsx
@@ -1,11 +1,21 @@
 import React from 'react'
 import Navbar from '../../components/Navbar'
+import './hero.scss'
 
 const Hero = () => {
   return (
-    <section>
+    <section className='heroSection'>
         <Navbar/>
-        This is hero section
+        <div className="imgContainer">
+          <img className='heroImg1' 
+          src="https://ik.imagekit.io/rhuubreuu/porsche-website/cosmos_382284989.webp" alt="heroSectionImg" loading='lazy' />
+          <img className='heroImg2'
+          src="https://ik.imagekit.io/rhuubreuu/porsche-website/cosmos_382284989.webp" alt="heroSectionImg" loading='lazy' />
+          <img className='heroImg3'
+          src="https://ik.imagekit.io/rhuubreuu/porsche-website/cosmos_1606437539.webp" alt="heroSectionImg" loading='lazy' />
+          <img className='heroImg4'
+          src="https://ik.imagekit.io/rhuubreuu/porsche-website/cosmos_1674367478.webp" alt="heroSectionImg" loading='lazy' />
+        </div>
     </section>
   )
 }
